8b1901e1556d23af91c24f482ed4dcaa3a62fe9f
Ravi <Ravi@DESKTOP-2Q1G09C>
2017-01-25 02:35:05 +0530

    Add project files.

---
+++ JenkinApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinApp.Startup))]
+namespace JenkinApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
